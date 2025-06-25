43369349cfb0c41ce741d840f194313a3949b004
rakesh <rakeshreddy@gmail.com>
2025-06-25 15:54:35 +0200

    added shipping

--- mysql.sh
+++ mysql.sh
@@ -21,7 +21,11 @@ then
     echo -e "$2.... $G success $N"
     fi
  }
-dnf install mysql-server -y &>>$LOG_FILE
+ yum module disable mysql -y 
+ validate $? "disable mysql"
+ cp /home/ec2-user/roboshop-shell-2025/mysql.repo /etc/yum.repos.d/mysql.repo
+ validate $? "copying mysql.repo"
+ yum install mysql-community-server -y &>>$LOG_FILE
 validate $? "installing mysql-server"
 systemctl enable mysqld &>>$LOG_FILE
 validate $? "enable mysql"
