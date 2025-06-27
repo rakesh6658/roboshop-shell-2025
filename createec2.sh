1b2c2b84cd55726581638db9a30063e87f5a7f91
rakesh <rakeshreddy@gmail.com>
2025-06-27 12:25:59 +0200

    added createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -2,7 +2,7 @@
 instances=("mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "web")
 echo "all instances ${instances[@]}"
 image_id="ami-09c813fb71547fc4f"
-instance_type=""
+
 security_group_id="sg-04b86a12980ec1b5f"
 for i in ${instances[$@]}
 do
