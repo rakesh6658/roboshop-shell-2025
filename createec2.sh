aaa153a69667c419eab6b7ce71417bc8b46bfaf7
rakesh <rakeshreddy@gmail.com>
2025-06-27 12:38:30 +0200

    added createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -6,16 +6,5 @@ image_id="ami-09c813fb71547fc4f"
 security_group_id="sg-04b86a12980ec1b5f"
 for i in "${instances[$@]}"
 do
-if [[ "$i" == "mongodb" || "$i" == "mysql" ]] 
-then
-instance_type="t3.medium"
-else
-instance_type="t2.micro"
-fi
-aws ec2 run-instances \
-  --image-id $image_id \
-  --instance-type $instance_type \
-  --security-group-ids $security_group_id \
-   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
-| jq '.Instances[].PrivateIpAddress'
+echo $i
 done
