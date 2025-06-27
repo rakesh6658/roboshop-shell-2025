8822d665dee04cd3de9b87a142c17823630151c9
rakesh <rakeshreddy@gmail.com>
2025-06-27 12:53:31 +0200

    added createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -2,6 +2,8 @@
 instances=("mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "web")
 echo "all instances ${instances[@]}"
 image_id="ami-09c813fb71547fc4f"
+hosted_zone_id="Z08250303NQOMHGBRQZIZ"
+domain_name="joindevops.store"
 
 security_group_id="sg-04b86a12980ec1b5f"
 for i in "${instances[@]}"
@@ -12,10 +14,28 @@ instance_type="t3.medium"
 else
 instance_type="t2.micro"
 fi
-aws ec2 run-instances \
+private_ip=$(aws ec2 run-instances \
   --image-id $image_id \
   --instance-type $instance_type \
   --security-group-ids $security_group_id \
    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
-| jq '.Instances[].PrivateIpAddress'
+| jq -r '.Instances[].PrivateIpAddress')
+aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch "{
+
+  '"Changes": [
+    {
+      "Action": "CREATE",
+      "ResourceRecordSet": {
+        "Name": "$i.$domain_name",
+        "Type": "A",
+        "TTL": 300,
+        "ResourceRecords": [
+          {
+            "Value": "$private_ip"
+          }
+        ]
+      }
+    }
+  ]'
+}"
 done
