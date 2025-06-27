33e876736bf94cd4ec2045a1f461b6bbd3228139
rakesh <rakeshreddy@gmail.com>
2025-06-27 13:01:55 +0200

    added createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
+
 instances=("mongodb" "catalogue" "redis" "user" "cart" "mysql" "shipping" "rabbitmq" "payment" "web")
-echo "all instances ${instances[@]}"
+
+echo "All instances: ${instances[@]}"
+
 image_id="ami-09c813fb71547fc4f"
 hosted_zone_id="Z08250303NQOMHGBRQZIZ"
 domain_name="joindevops.store"
-
 security_group_id="sg-04b86a12980ec1b5f"
+
 for i in "${instances[@]}"
 do
-  if [[ "$i" == "mongodb" || "$i" == "mysql" ]] 
-then
-instance_type="t3.medium"
-else
-instance_type="t2.micro"
-fi
-private_ip=$(aws ec2 run-instances \
-  --image-id $image_id \
-  --instance-type $instance_type \
-  --security-group-ids $security_group_id \
-   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
-| jq  '.Instances[].PrivateIpAddress')
-aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --change-batch "{
+  if [[ "$i" == "mongodb" || "$i" == "mysql" ]]; then
+    instance_type="t3.medium"
+  else
+    instance_type="t2.micro"
+  fi
 
-  '"Changes": [
+  echo "Launching $i with instance type $instance_type"
+
+  private_ip=$(aws ec2 run-instances \
+    --image-id "$image_id" \
+    --instance-type "$instance_type" \
+    --security-group-ids "$security_group_id" \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" \
+    | jq -r '.Instances[].PrivateIpAddress')
+
+  echo "$i private IP: $private_ip"
+
+  # Route53 record creation with safe variable expansion
+  aws route53 change-resource-record-sets --hosted-zone-id "$hosted_zone_id" --change-batch "$(cat <<EOF
+{
+  "Changes": [
     {
       "Action": "CREATE",
       "ResourceRecordSet": {
@@ -36,6 +45,9 @@ aws route53 change-resource-record-sets --hosted-zone-id $hosted_zone_id --chang
         ]
       }
     }
-  ]'
-}"
+  ]
+}
+EOF
+)"
+
 done
