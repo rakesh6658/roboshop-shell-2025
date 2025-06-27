da7923c16a279f06755d0952229145a3b557827c
rakesh <rakeshreddy@gmail.com>
2025-06-27 12:40:20 +0200

    added createec2.sh

--- createec2.sh
+++ createec2.sh
@@ -4,7 +4,6 @@ echo "all instances ${instances[@]}"
 image_id="ami-09c813fb71547fc4f"
 
 security_group_id="sg-04b86a12980ec1b5f"
-for i in "${instances[$@]}"
-do
-echo $i
-done
+while read line
+do 
+done <<< ${instances[$@]}
